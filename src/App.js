33b96e444b2030d6b38a9993a3bd5b78d1fa61ec
Medul Hasan <medul.cse@gmail.com>
2021-10-18 16:08:01 +0600

    work on authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import AuthProvider from './context/AuthProvider';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import SignIn from './Pages/SignIn/SignIn';
+import Signup from './Pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { HashLink } from 'react-router-hash-link';
+
+import Brand from '../../../images/brand/brand.png';
+import './Header.css';
+import useAuth from '../../../hooks/useAuth';
+
+const Header = () => {
+    const { user, logOut } = useAuth();
+    return (
+        <Navbar className="header" bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={HashLink} to="/">
+                    <img style={{ width: '100px' }} src={Brand} alt="" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={HashLink} to="/">Home</Nav.Link>
+                        {
+                            user?.email ? (
+                                <>
+                                    <Nav.Link className="logout mx-3" as={HashLink} to="/" onClick={logOut}>Logout</Nav.Link>
+                                    <Navbar.Text>
+                                        Signed in as: <a href="#login">{user.displayName && user.displayName}</a>
+                                    </Navbar.Text>
+                                </>
+                            ) : (
+                                <>
+                                    <Nav.Link as={HashLink} to="/signin">Sign in</Nav.Link>
+                                    <Nav.Link className="sign-up" as={HashLink} to="/signup">Sign up</Nav.Link>
+                                </>
+                            )
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
+    );
+};
+
+export default Header;
